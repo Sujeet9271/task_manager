5667024c8ec09962002c154176d4cf9f22df2044
Sujeet Kumar Gupta <sujeet9271@gmail.com>
2025-04-08 22:09:32 +0545

    fix: Update user activation logic in UserManager and Users model

--- accounts/models.py
+++ accounts/models.py
@@ -10,16 +10,15 @@ class UserManager(BaseUserManager):
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
-        if not password:
+        if password:
             user.set_password(password)
-            user.is_active=False
         user.save(using=self._db)
         return user
 
     def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_active', True)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-
         return self.create_user(email, password, **extra_fields)
     
 
@@ -27,7 +26,7 @@ class Users(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=30, unique=True)
     name = models.CharField(max_length=30, blank=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     date_joined = models.DateTimeField(auto_now_add=True)
 
