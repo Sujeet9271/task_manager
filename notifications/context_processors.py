34df91bc47b9aa8a058b76201eb16da800a7cd0d
Sujeet Kumar Gupta <sujeet9271@gmail.com>
2025-04-13 16:40:08 +0545

    updated notification context processor

--- notifications/context_processors.py
+++ notifications/context_processors.py
@@ -3,9 +3,10 @@ from notifications.utils import get_notifications
 
 def notifications(request):
     context = {}
-    if request.user.is_authenticated:
-        context['unread_notification_count'] = request.user.notifications.filter(read=False).count()
-        context = get_notifications(user=request.user,page_number=1, context=context)
-    else:
-        context['notifications'] = []
+    if not request.htmx:
+        if request.user.is_authenticated:
+            context['unread_notification_count'] = request.user.notifications.filter(read=False).count()
+            context = get_notifications(user=request.user,page_number=1, context=context)
+        else:
+            context['notifications'] = []
     return context
